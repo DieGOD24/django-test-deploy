f1668bb3be9b55583541a5de9cafb147d09f13a5
DieGOD24 <diego.giraldo2@utp.edu.co>
2025-09-22 03:13:37 -0500

    feat: news 21 d

--- postgresTest/urls.py
+++ postgresTest/urls.py
@@ -23,6 +23,3 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
